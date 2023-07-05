ed3751f55f78e75221245e6ad1dbb9ede462be3a
Manish <114460202+Manish4567jkl@users.noreply.github.com>
2023-07-05 15:25:44 +0530

    Update  FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,7 +1,8 @@
 //FIZZBUZZ
 //FIZZ FOR MULTIPLE OF 3.BUZZ FOR MULTIPLES OF 5,FIZZBUZZ FOR MULTIPLES OF BOTH 3 AND 5.
 let i ;
-for(i=1;i<100;i++){
+let number = prompt("enter your number :");
+for(i=1;i<=number;i++){
      if(i % 3 === 0 && i % 5 === 0){
         console.log("FIZZBUZZ");
     }
